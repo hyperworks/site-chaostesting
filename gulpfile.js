a59dcd5f05300276752e60b17deffc7599cfd405
Matthew Kanwisher <matt@kanwisher.com>
2015-09-16 05:42:27 +0700

    testing stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,38 @@ gulp.task('default', function() {
 
 var awspublish = require('gulp-awspublish');
  
-gulp.task('publish', function() {
+gulp.task('publish_chaosgopher', function() {
  
   // create a new publisher 
   var publisher = awspublish.create({params: { Bucket: 'chaosgopher.com' }});
+ 
+  // define custom headers 
+  var headers = {
+     'Cache-Control': 'max-age=315360000, no-transform, public'
+     // ... 
+   };
+ 
+  gulp.src('./static/**')
+ 
+     // gzip, Set Content-Encoding headers and add .gz extension 
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+ 
+    // publisher will add Content-Length, Content-Type and headers specified above 
+    // If not specified it will set x-amz-acl to public-read by default 
+    .pipe(publisher.publish(headers))
+ 
+    // create a cache file to speed up consecutive uploads 
+    .pipe(publisher.cache())
+ 
+     // print upload updates to console 
+    .pipe(awspublish.reporter());
+});
+
+
+gulp.task('publish_chaostesting', function() {
+ 
+  // create a new publisher 
+  var publisher = awspublish.create({params: { Bucket: 'chaostesting.com' }});
  
   // define custom headers 
   var headers = {
--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #npm install --save-dev gulp
 #npm install --save-dev gulp-awspublish
-#./node_modules/gulp/bin/gulp.js publish_chaosgopher
-./node_modules/gulp/bin/gulp.js publish #_chaostesting
+./node_modules/gulp/bin/gulp.js publish_chaosgopher
+./node_modules/gulp/bin/gulp.js publish_chaostesting
