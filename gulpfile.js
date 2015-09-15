6ccd562f0416fdc849ce5e841696fe8d01fde216
Matthew Kanwisher <matt@kanwisher.com>
2015-09-16 05:31:25 +0700

    testing stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,10 @@ gulp.task('default', function() {
 
 var awspublish = require('gulp-awspublish');
  
-gulp.task('publish_chaostesting', function() {
+gulp.task('publish', function() {
  
   // create a new publisher 
-  var publisher = awspublish.create({ Bucket: 'chaostesting.com' });
- 
-  // define custom headers 
-  var headers = {
-     'Cache-Control': 'max-age=315360000, no-transform, public'
-     // ... 
-   };
- 
-  gulp.src('./static/**')
- 
-     // gzip, Set Content-Encoding headers and add .gz extension 
-    .pipe(awspublish.gzip({ ext: '.gz' }))
- 
-    // publisher will add Content-Length, Content-Type and headers specified above 
-    // If not specified it will set x-amz-acl to public-read by default 
-    .pipe(publisher.publish(headers))
- 
-    // create a cache file to speed up consecutive uploads 
-    .pipe(publisher.cache())
- 
-     // print upload updates to console 
-    .pipe(awspublish.reporter());
-});
-
-
-gulp.task('publish_chaosgopher', function() {
- 
-  // create a new publisher 
-  var publisher = awspublish.create({ Bucket: 'chaosgopher.com' });
+  var publisher = awspublish.create({ Bucket: 'chaosgopher.com', bucket 'chaosgopher.com' });
  
   // define custom headers 
   var headers = {
